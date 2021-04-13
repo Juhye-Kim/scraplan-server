fe7ac6c61516c9f826c07859f67185b4ab1095a8
yubin-j <developersurfer@gmail.com>
2021-04-13 22:49:19 +0900

    resolved #17

--- controllers/curation/deleteCuration.js
+++ controllers/curation/deleteCuration.js
@@ -1,1 +1,32 @@
-module.exports = async () => {};
+const { Curation, Sequelize } = require("../../models");
+
+module.exports = async (req, res) => {
+  const { authData } = req;
+  const { id } = req.body;
+
+  if (!authData || !id || typeof id !== "number") {
+    return res.status(400).json({ message: "Insufficient info" });
+  }
+
+  if (!authData.isAdmin) {
+    return res.status(403).send();
+  }
+
+  try {
+    const curationInfo = await Curation.findOne({ where: { id } });
+
+    if (curationInfo) {
+      await curationInfo.destroy();
+      res.status(200).json({ message: "successfully deleted" });
+    } else {
+      res.status(404).json({ message: "There is no data with given id" });
+    }
+  } catch (err) {
+    console.log(
+      "-------------------------------Error occurred in curation/deleteCurations.js-------------------------------- \n",
+      err,
+      "-------------------------------Error occurred in curation/deleteCurations.js-------------------------------- \n"
+    );
+    res.status(500).send();
+  }
+};
